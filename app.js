07dbdc4b3de97f37c2ec2b6fbfa2339f62b1e6a5
Lara Potjewyd <larapotjewyd@gmail.com>
2019-03-27 08:26:14 -0600

    fixed scroll

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const navbar = document.getElementsByClassName('navbar')[0]
 
 scrollNav = () => {
-  if(window.pageYOffset > 718) {
+  if(window.pageYOffset > window.innerHeight) {
     navbar.classList.add('scrolled')
-  } else if (window.pageYOffset < 718) {
+  } else if (window.pageYOffset < window.innerHeight) {
     navbar.classList.remove('scrolled')
   }
 }
