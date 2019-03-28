d01ddb9a42e828e8b27c56e0fc44a51f3f3f87b1
Lara Potjewyd <larapotjewyd@gmail.com>
2019-03-28 10:24:06 -0600

    added smooth scroll

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const navbar = document.getElementsByClassName('navbar')[0]
+const about = document.getElementById('about')
+const skills = document.getElementById('skills')
+const portfolio = document.getElementById('portfolio')
+const contact = document.getElementById('contact')
 
 scrollNav = () => {
   if(window.pageYOffset > window.innerHeight) {
@@ -6,4 +10,20 @@ scrollNav = () => {
   } else if (window.pageYOffset < window.innerHeight) {
     navbar.classList.remove('scrolled')
   }
+}
+
+scrollToAbout = () => {
+  about.scrollIntoView(true)
+}
+
+scrollToSkills = () => {
+  skills.scrollIntoView(true)
+}
+
+scrollToPortfolio = () => {
+  portfolio.scrollIntoView(true)
+}
+
+scrollToContact = () => {
+  contact.scrollIntoView(true)
 }
