3f4303d296ec12667e5b5e0563d154107e2f2483
Lara Potjewyd <larapotjewyd@gmail.com>
2019-07-26 00:03:28 -0600

    changed cv

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const navbar = document.getElementsByClassName('navbar')[0]
-const about = document.getElementById('about')
-const skills = document.getElementById('skills')
-const portfolio = document.getElementById('portfolio')
-const contact = document.getElementById('contact')
+const navbar = document.getElementsByClassName("navbar")[0];
+const title = document.getElementsByClassName("title")[0];
+const about = document.getElementById("about");
+const skills = document.getElementById("skills");
+const portfolio = document.getElementById("portfolio");
+const contact = document.getElementById("contact");
 
 scrollNav = () => {
-  if(window.pageYOffset > window.innerHeight) {
-    navbar.classList.add('scrolled')
+  if (window.pageYOffset > window.innerHeight) {
+    navbar.classList.add("scrolled");
+    title.classList.remove("scrolled");
   } else if (window.pageYOffset < window.innerHeight) {
-    navbar.classList.remove('scrolled')
+    navbar.classList.remove("scrolled");
+    title.classList.add("scrolled");
   }
-}
+};
 
 scrollToAbout = () => {
-  about.scrollIntoView(true)
-}
+  about.scrollIntoView(true);
+};
 
 scrollToSkills = () => {
-  skills.scrollIntoView(true)
-}
+  skills.scrollIntoView(true);
+};
 
 scrollToPortfolio = () => {
-  portfolio.scrollIntoView(true)
-}
+  portfolio.scrollIntoView(true);
+};
 
 scrollToContact = () => {
-  contact.scrollIntoView(true)
-}
+  contact.scrollIntoView(true);
+};
